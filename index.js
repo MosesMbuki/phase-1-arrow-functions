85f057fca1b915b24435e84afdde1fd44cb5fd3e
Moses Mbuki Mutitu <moses.mutitu@student.moringaschool.com>
2025-03-08 23:35:29 +0300

    assignment complete

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. divide
+const divide = function (a = 2000, b = 100) {
+    return a / b;
+  };
+  
+  // 2. square
+  const square = (x) => x * x;
+  
+  // 3. add
+  const add = (a, b) => a + b;
